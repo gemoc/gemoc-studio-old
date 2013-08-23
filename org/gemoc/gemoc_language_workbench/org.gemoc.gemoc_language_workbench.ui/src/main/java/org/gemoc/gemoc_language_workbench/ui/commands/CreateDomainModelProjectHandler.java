203260fab73abdc8b67076903a8ce42c3a73d2ad
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-08-23 09:34:47 +0200

    starting refactoring for simplification of CommandHandler code

--- org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.ui/src/main/java/org/gemoc/gemoc_language_workbench/ui/commands/CreateDomainModelProjectHandler.java
+++ org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.ui/src/main/java/org/gemoc/gemoc_language_workbench/ui/commands/CreateDomainModelProjectHandler.java
@@ -1,73 +1,28 @@
 package org.gemoc.gemoc_language_workbench.ui.commands;
 
-import java.util.Iterator;
-
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.gemoc.gemoc_language_workbench.ui.wizards.CreateDomainModelWizard;
+//import org.eclipse.jface.dialogs.MessageDialog;
 
-public class CreateDomainModelProjectHandler extends AbstractHandler implements
+public class CreateDomainModelProjectHandler extends AbstractGemocLanguageProjectHandler implements
 		IHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		// get the optional selection and eventually project data to preset the wizard
+		IProject updatedGemocLanguageProject = getUpdateGemocLanguageProjectFromSelection(event);
 
-		IProject updatedGemocLanguageProject=null;
-		
-		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
-			        .getActivePage().getSelection();
-			    if (selection != null & selection instanceof IStructuredSelection) {
-			      IStructuredSelection strucSelection = (IStructuredSelection) selection;
-			      for (Iterator<Object> iterator = strucSelection.iterator(); iterator
-			          .hasNext();) {
-			        Object element = iterator.next();
-			        
-			        if(element  instanceof IResource) {
-			        	updatedGemocLanguageProject =((IResource)element).getProject();
-						
-					}
-			        if(element  instanceof IAdaptable){
-			        	IResource res =(IResource) ((IAdaptable)element).getAdapter(IResource.class);
-			        	if(res != null){
-			        		updatedGemocLanguageProject = res.getProject();
-			        	}
-			        }
-			        
-			        // if still null look after the activeEditor
-			        if(updatedGemocLanguageProject == null){
-			        	
-			        	IResource res =(IResource) HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getActiveEditor().getEditorInput().getAdapter(IResource.class);
-			        	if(res != null){
-			        		updatedGemocLanguageProject = res.getProject();
-			        	}
-			        }
-			        
-			        
-			   /*     MessageDialog.openInformation(
-							HandlerUtil.getActiveWorkbenchWindow(event).getShell(),
-							"Gemoc Language Workbench UI",
-							"Create Domain Model Project command was executed. Selected elment ="+element.toString());
-			     */   
-			      }
-			    }
-		
-	
-		// launch the wizard that will select the action	    
-	    WizardDialog wizardDialog = new WizardDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell(), new CreateDomainModelWizard(updatedGemocLanguageProject));
+		// launch the wizard that will select the action and do the job
+		WizardDialog wizardDialog = new WizardDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell(),
+													 new CreateDomainModelWizard(updatedGemocLanguageProject));
 		wizardDialog.open();
-		
+
 		return null;
 	}
 
