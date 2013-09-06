586bc45e04e89171808bce4000807cc043eb5e0b
Florent Latombe <flatombe@gmail.com>
2013-09-06 09:22:04 +0200

    deleted plugins which have been refactored directly in the engine plugin

--- org/gemoc/execution/engine/org.gemoc.execution.engine.executors.emf.configuration/src/fr/inria/aoste/timesquare/backend/serializer/EmfExecutionConfigurationSemanticSequencer.java
+++
@@ -1,5 +0,0 @@
-package fr.inria.aoste.timesquare.backend.serializer;
-
-// public class EmfExecutionConfigurationSemanticSequencer extends
-// AbstractEmfExecutionConfigurationSemanticSequencer {
-// }
--- org/gemoc/execution/engine/org.gemoc.execution.engine.executors.emf.configuration/src/fr/inria/aoste/timesquare/backend/serializer/EmfExecutionConfigurationSyntacticSequencer.java
+++
@@ -1,5 +0,0 @@
-package fr.inria.aoste.timesquare.backend.serializer;
-
-// public class EmfExecutionConfigurationSyntacticSequencer extends
-// AbstractEmfExecutionConfigurationSyntacticSequencer {
-// }
