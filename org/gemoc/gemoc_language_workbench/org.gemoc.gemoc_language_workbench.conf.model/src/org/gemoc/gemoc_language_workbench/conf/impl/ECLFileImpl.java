30ef4dc3fc461f2715e08adfe68bbd0598b34846
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-09-19 10:56:48 +0200

    added ECLFile resource in conf model

---
+++ org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.conf.model/src/org/gemoc/gemoc_language_workbench/conf/impl/ECLFileImpl.java
@@ -0,0 +1,41 @@
+/**
+ */
+package org.gemoc.gemoc_language_workbench.conf.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.gemoc.gemoc_language_workbench.conf.ECLFile;
+import org.gemoc.gemoc_language_workbench.conf.confPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>ECL File</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ECLFileImpl extends EObjectImpl implements ECLFile {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ECLFileImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return confPackage.Literals.ECL_FILE;
+	}
+
+} //ECLFileImpl
