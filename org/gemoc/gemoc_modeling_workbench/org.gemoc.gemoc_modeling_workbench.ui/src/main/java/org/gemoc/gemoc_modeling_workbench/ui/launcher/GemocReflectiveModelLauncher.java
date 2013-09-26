2d125d68005e419a05dd5c29a1f221b951bce4d4
Didier Vojtisek <didier.vojtisek@inria.fr>
2013-09-26 18:10:17 +0200

    better feedback when failing to launch a model due to missing info in the language definition

--- org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/GemocReflectiveModelLauncher.java
+++ org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/GemocReflectiveModelLauncher.java
@@ -23,15 +23,18 @@ public class GemocReflectiveModelLauncher implements ILaunchConfigurationDelegat
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
 			throws CoreException {
 
-		Activator.getDefault().getMessaggingSystem().showConsole();
-		Activator
+		Activator.getDefault().getMessaggingSystem().showConsole();		
+		/*Activator
 				.getDefault()
 				.getMessaggingSystem()
 				.warn("Run Gemoc Model using MiniEngine for test, need to connect the real generic execution engine here",
-						"");
+						"");*/
 		String modelPath = configuration.getAttribute(GemocModelLauncherConfigurationConstants.LAUNCH_MODEL_PATH, "");
 		String languageName = configuration.getAttribute(
 				GemocModelLauncherConfigurationConstants.LAUNCH_SELECTED_LANGUAGE, "");
+		
+		Activator.getDefault().getMessaggingSystem()
+			.info("Run \""+modelPath+"\" using \""+languageName+"\" language definition...", "");
 
 		IConfigurationElement confElement = null;
 		IConfigurationElement[] confElements = Platform.getExtensionRegistry().getConfigurationElementsFor(
@@ -52,42 +55,47 @@ public class GemocReflectiveModelLauncher implements ILaunchConfigurationDelegat
 
 		// get the extension objects
 		if (confElement != null) {
-			final Object oinitializer = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_INITIALIZER_ATT);
-			if (oinitializer instanceof LanguageInitializer) {
-				languageInitializer = (LanguageInitializer) oinitializer;
+			try{
+				final Object oinitializer = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_INITIALIZER_ATT);
+				if (oinitializer instanceof LanguageInitializer) {
+					languageInitializer = (LanguageInitializer) oinitializer;
+				}
+	
+				final Object omodelLoader = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_LOADMODEL_ATT);
+				if (omodelLoader instanceof ModelLoader) {
+					modelLoader = (ModelLoader) omodelLoader;
+				}
+	
+				final Object oSolver = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_SOLVER_ATT);
+				if (oSolver instanceof Solver) {
+					solver = (Solver) oSolver;
+				}
+	
+				final Object oexecutor = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_EXECUTOR_ATT);
+				if (oexecutor instanceof Executor) {
+					executor = (Executor) oexecutor;
+				}
+	
+				final Object oFeedbackPolicy = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_FEEDBACKPOLICY_ATT);
+				if (oFeedbackPolicy instanceof FeedbackPolicy) {
+					feedbackPolicy = (FeedbackPolicy) oFeedbackPolicy;
+				}
+				
+				final Object oEclFilePath = confElement
+						.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_ECL_FILE_PATH_ATT);
+				if (oEclFilePath instanceof String) {
+					eclFilePath = (String) oEclFilePath;
+				}
+			}catch(CoreException ce){
+				Activator.getDefault()
+					.getMessaggingSystem().error("Cannot run model using language \""+languageName+"\" because the language isn't fully defined. "+ce.getMessage(), "");
+				throw ce;
 			}
-
-			final Object omodelLoader = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_LOADMODEL_ATT);
-			if (omodelLoader instanceof ModelLoader) {
-				modelLoader = (ModelLoader) omodelLoader;
-			}
-
-			final Object oSolver = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_SOLVER_ATT);
-			if (oSolver instanceof Solver) {
-				solver = (Solver) oSolver;
-			}
-
-			final Object oexecutor = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_EXECUTOR_ATT);
-			if (oexecutor instanceof Executor) {
-				executor = (Executor) oexecutor;
-			}
-
-			final Object oFeedbackPolicy = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_FEEDBACKPOLICY_ATT);
-			if (oFeedbackPolicy instanceof FeedbackPolicy) {
-				feedbackPolicy = (FeedbackPolicy) oFeedbackPolicy;
-			}
-			
-			final Object oEclFilePath = confElement
-					.createExecutableExtension(org.gemoc.gemoc_language_workbench.ui.Activator.GEMOC_LANGUAGE_EXTENSION_POINT_XDSML_DEF_ECL_FILE_PATH_ATT);
-			if (oEclFilePath instanceof String) {
-				eclFilePath = (String) oEclFilePath;
-			}
-			
 		}
 
 		if (languageInitializer == null | modelLoader == null | solver == null | executor == null
