675694e6b1c32bbe062e1a0739da70f59b551331
Florent Latombe <flatombe@gmail.com>
2013-09-26 17:55:24 +0200

    improve k2 language initializer

--- org/gemoc/execution/engine/org.gemoc.execution.engine/src/org/gemoc/execution/engine/api_implementations/utils/Kermeta2LanguageInitializer.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine/src/org/gemoc/execution/engine/api_implementations/utils/Kermeta2LanguageInitializer.java
@@ -22,106 +22,109 @@ import org.gemoc.execution.engine.Activator;
 import org.gemoc.gemoc_language_workbench.api.utils.LanguageInitializer;
 
 public class Kermeta2LanguageInitializer implements LanguageInitializer {
-	
+
+	String jarDependenciesFolderPath;
+	String jarDsaFolderPath;
+
+	public Kermeta2LanguageInitializer(String jarDependenciesFolderPath, String jarDsaFolderPath) {
+		this.jarDependenciesFolderPath = jarDependenciesFolderPath;
+		this.jarDsaFolderPath = jarDsaFolderPath;
+	}
 
 	@Override
-	public void initialize() {		
+	public void initialize() {
+		Activator.getMessagingSystem().info("Initializing Kermeta2 Language with...", Activator.PLUGIN_ID);
+		Activator.getMessagingSystem().info("\tFolder containing jar dependencies: " + jarDependenciesFolderPath,
+				Activator.PLUGIN_ID);
+		Activator.getMessagingSystem().info("\tFolder containing jar DSA: " + jarDsaFolderPath, Activator.PLUGIN_ID);
 		Activator.getMessagingSystem().debug(
-                "Adding all the JAR files from folder " + jarDependenciesFolderPath + " and " + jarDsaFolderPath
-                        + " to the current ClassLoader", Activator.PLUGIN_ID);
-        ClassLoader customizedClassLoader = this.customizeClassLoader(jarDsaFolderPath, jarDependenciesFolderPath);
-        Activator.getMessagingSystem().debug("Initializing the model loader", Activator.PLUGIN_ID);
-        EObject modelLoader = this.createAndInitializeModelLoader(customizedClassLoader);
+				"Adding all the JAR files from folder " + jarDependenciesFolderPath + " and " + jarDsaFolderPath
+						+ " to the current ClassLoader", Activator.PLUGIN_ID);
+		ClassLoader customizedClassLoader = this.customizeClassLoader(jarDsaFolderPath, jarDependenciesFolderPath);
+		this.initializeKermeta2(customizedClassLoader);
 	}
-	
-	private List<URL> getJarUrlsFromFolder(String folderPath) throws MalformedURLException, CoreException {
-        List<URL> urls = new ArrayList<URL>();
-        IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getFolder(new Path(folderPath));
-        for (IResource resource : folder.members()) {
-            if (resource instanceof IFile) {
-                IFile file = (IFile) resource;
-                if (file.getFileExtension().equals("jar")) {
-                    urls.add(file.getLocationURI().toURL());
-                }
-            }
-        }
-        return urls;
-    }
+
+	private void initializeKermeta2(ClassLoader classLoader) {
+		if (classLoader == null) {
+			throw new NullPointerException("The given ClassLoader is null");
+		}
+
+		// Parsing the "main.properties" file.
+		Properties prop = new Properties();
+		InputStream in = classLoader.getResourceAsStream("main.properties");
+		try {
+			prop.load(in);
+			Class<?> init;
+			init = classLoader.loadClass(prop.get("mainRunner").toString());
+			Activator.getMessagingSystem().debug(
+					"Calling method init4eclipse : " + init.getDeclaredMethod("init4eclipse").toString(),
+					Activator.PLUGIN_ID);
+			init.getDeclaredMethod("init4eclipse").invoke(null, (Object[]) null);
+			in.close();
+		} catch (IOException e) {
+			String errorMessage = "IOException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (ClassNotFoundException e) {
+			String errorMessage = "ClassNotFoundException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (IllegalAccessException e) {
+			String errorMessage = "IllegalAccessException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (IllegalArgumentException e) {
+			String errorMessage = "IllegalArgumentException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (InvocationTargetException e) {
+			String errorMessage = "InvocationTargetException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+			Activator.error(e.getCause().getMessage(), e.getCause());
+		} catch (NoSuchMethodException e) {
+			String errorMessage = "NoSuchMethodException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (SecurityException e) {
+			String errorMessage = "SecurityException while initializing the Kermeta2 language";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		}
+
+	}
+
 	private ClassLoader customizeClassLoader(String jarDsaFolderPath, String jarDependenciesFolderPath) {
-        List<URL> urls = new ArrayList<URL>();
-        try {
-            urls.addAll(this.getJarUrlsFromFolder(jarDependenciesFolderPath));
-            urls.addAll(this.getJarUrlsFromFolder(jarDsaFolderPath));
-        } catch (CoreException e) {
-            String errorMessage = "CoreException while customizing the ClassLoader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (MalformedURLException e) {
-            String errorMessage = "MalformedURLException while customizing the ClassLoader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        }
+		List<URL> urls = new ArrayList<URL>();
+		try {
+			urls.addAll(this.getJarUrlsFromFolder(jarDependenciesFolderPath));
+			urls.addAll(this.getJarUrlsFromFolder(jarDsaFolderPath));
+		} catch (CoreException e) {
+			String errorMessage = "CoreException while customizing the ClassLoader";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		} catch (MalformedURLException e) {
+			String errorMessage = "MalformedURLException while customizing the ClassLoader";
+			Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
+			Activator.error(errorMessage, e);
+		}
+		URL[] turls = (URL[]) urls.toArray(new URL[urls.size()]);
+		ClassLoader res = new URLClassLoader(turls, Activator.class.getClassLoader());
+		return res;
+	}
 
-        Activator.getMessagingSystem().debug("URLs added to the ClassLoader : " + urls.toString(), Activator.PLUGIN_ID);
-
-        URL[] turls = (URL[]) urls.toArray(new URL[urls.size()]);
-        ClassLoader res = new URLClassLoader(turls, Activator.class.getClassLoader());
-        return res;
-    }
-	
-	private EObject createAndInitializeModelLoader(ClassLoader classLoader) {
-        if (classLoader == null) {
-            throw new NullPointerException("The given ClassLoader is null");
-        }
-
-        // Parsing the "main.properties" file.
-        Properties prop = new Properties();
-        InputStream in = classLoader.getResourceAsStream("main.properties");
-        EObject modelLoader = null;
-        try {
-            prop.load(in);
-            Class<?> init;
-            init = classLoader.loadClass(prop.get("mainRunner").toString());
-            Activator.getMessagingSystem().debug(
-                    "ClassLoader : " + Arrays.asList(((URLClassLoader) classLoader).getURLs()).toString(),
-                    Activator.PLUGIN_ID);
-            Activator.getMessagingSystem().debug(
-                    "Method init4eclipse : " + init.getDeclaredMethod("init4eclipse").toString(), Activator.PLUGIN_ID);
-            init.getDeclaredMethod("init4eclipse").invoke(null, (Object[]) null);
-            in.close();
-        } catch (IOException e) {
-            String errorMessage = "IOException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (ClassNotFoundException e) {
-            String errorMessage = "ClassNotFoundException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (IllegalAccessException e) {
-            String errorMessage = "IllegalAccessException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (IllegalArgumentException e) {
-            String errorMessage = "IllegalArgumentException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (InvocationTargetException e) {
-            String errorMessage = "InvocationTargetException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-            Activator.error(e.getCause().getMessage(), e.getCause());
-        } catch (NoSuchMethodException e) {
-            String errorMessage = "NoSuchMethodException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        } catch (SecurityException e) {
-            String errorMessage = "SecurityException while initializing the model loader";
-            Activator.getMessagingSystem().error(errorMessage, Activator.PLUGIN_ID);
-            Activator.error(errorMessage, e);
-        }
-
-        return modelLoader;
-
-    }
+	private List<URL> getJarUrlsFromFolder(String folderPath) throws MalformedURLException, CoreException {
+		List<URL> urls = new ArrayList<URL>();
+		IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getFolder(new Path(folderPath));
+		for (IResource resource : folder.members()) {
+			if (resource instanceof IFile) {
+				IFile file = (IFile) resource;
+				if (file.getFileExtension().equals("jar")) {
+					urls.add(file.getLocationURI().toURL());
+				}
+			}
+		}
+		return urls;
+	}
 
 }
