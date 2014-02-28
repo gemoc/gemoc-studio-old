bb59a296fa762c1c5b3dee542ee4e87f872d95cc
screff <screff@scm.gforge.inria.fr>
2014-02-28 15:38:16 +0100

    mwe2 options updated to be conform to the ccsl textual syntax

---
+++ org/gemoc/MoCC/concreteSyntax/textual/org.gemoc.mocc.ccslmocc.model.xtext.mocdsl/src/org/gemoc/mocc/ccslmocc/model/xtext/Activator.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2012-2016 GEMOC consortium.
+ * 
+ * http://www.gemoc.org
+ * 
+ * Contributors:
+ *   Stephen Creff - ENSTA Bretagne [stephen.creff@ensta-bretagne.fr]
+ *   
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * $Id$
+ */
+package org.gemoc.mocc.ccslmocc.model.xtext;
+ 
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
+
+
