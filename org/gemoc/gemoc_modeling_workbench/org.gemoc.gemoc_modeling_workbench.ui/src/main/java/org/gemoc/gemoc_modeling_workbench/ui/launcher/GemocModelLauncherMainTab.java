68db3413037cfd36facf36fe469a2433497bf4ec
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-03-26 12:24:35 +0100

    fixes merge problem

--- org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/GemocModelLauncherMainTab.java
+++ org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/GemocModelLauncherMainTab.java
@@ -204,11 +204,7 @@ public class GemocModelLauncherMainTab extends AbstractLaunchConfigurationTab {
 				// handleModelLocationButtonSelected();
 				// TODO launch the appropriate selector
 
-<<<<<<< HEAD
 				SelectAIRDIFileDialog dialog = new SelectAIRDIFileDialog(
-=======
-				SelectAnyIFileDialog dialog = new SelectAnyIFileDialog(
->>>>>>> refs/remotes/origin/master
 						PlatformUI.getWorkbench().getActiveWorkbenchWindow()
 								.getShell());
 				if (dialog.open() == Dialog.OK) {
