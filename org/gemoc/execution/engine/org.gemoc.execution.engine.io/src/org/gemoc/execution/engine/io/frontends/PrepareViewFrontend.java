f338181d0fb36b186d578fb457583210e8db74b0
ftanguy <francois.tanguy@inria.fr>
2014-06-03 16:14:35 +0200

    Forgot to add resource.

---
+++ org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/frontends/PrepareViewFrontend.java
@@ -0,0 +1,22 @@
+package org.gemoc.execution.engine.io.frontends;
+
+import org.eclipse.ui.PlatformUI;
+import org.gemoc.execution.engine.io.core.Frontend;
+import org.gemoc.execution.engine.io.views.EnginesStatusView;
+import org.gemoc.execution.engine.io.views.ExecutionTraceView;
+import org.gemoc.gemoc_commons.ui.ViewHelper;
+import org.gemoc.gemoc_language_workbench.api.core.GemocExecutionEngine;
+
+public class PrepareViewFrontend implements Frontend 
+{
+
+	@Override
+	public void initialize(GemocExecutionEngine engine) {
+		if (PlatformUI.isWorkbenchRunning())
+		{
+			ViewHelper.retrieveView(EnginesStatusView.ID);
+			ViewHelper.retrieveView(ExecutionTraceView.ID);
+		}
+	}
+
+}
