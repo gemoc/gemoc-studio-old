c246ee7397bbbaa5f0f6f4032886dae2d9e1c37a
ftanguy <francois.tanguy@inria.fr>
2014-06-11 16:43:51 +0200

    early phase of timeline integration from Obeo

--- org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/frontends/PrepareViewFrontend.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/frontends/PrepareViewFrontend.java
@@ -2,10 +2,14 @@ package org.gemoc.execution.engine.io.frontends;
 
 import org.eclipse.ui.PlatformUI;
 import org.gemoc.commons.eclipse.ui.ViewHelper;
+import org.gemoc.execution.engine.core.ObservableBasicExecutionEngine;
 import org.gemoc.execution.engine.io.core.Frontend;
 import org.gemoc.execution.engine.io.views.EnginesStatusView;
 import org.gemoc.execution.engine.io.views.ExecutionTraceView;
+import org.gemoc.execution.engine.io.views.obeo.TimelineProvider;
 import org.gemoc.gemoc_language_workbench.api.core.GemocExecutionEngine;
+
+import fr.obeo.timeline.view.TimelineView;
 
 public class PrepareViewFrontend implements Frontend 
 {
@@ -16,6 +20,9 @@ public class PrepareViewFrontend implements Frontend
 		{
 			ViewHelper.retrieveView(EnginesStatusView.ID);
 			ViewHelper.retrieveView(ExecutionTraceView.ID);
+			TimelineView timelineView = ViewHelper.retrieveView(TimelineView.ID);			
+			ObservableBasicExecutionEngine e = (ObservableBasicExecutionEngine) engine;
+			timelineView.setProvider(new TimelineProvider(e));
 		}
 	}
 
