cdf007d891a2752411a17d60e50b6b32dac8dc14
ftanguy <francois.tanguy@inria.fr>
2014-07-03 14:51:10 +0200

    use switch instead of ifs

--- org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/LogicalStepDeciderFactory.java
+++ org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/launcher/LogicalStepDeciderFactory.java
@@ -11,23 +11,30 @@ public class LogicalStepDeciderFactory {
 
 	public static ILogicalStepDecider CreateDecider(String deciderKind, Solver solver) {
 		ILogicalStepDecider decider = null;
-		if (deciderKind.equals(GemocModelLauncherConfigurationConstants.DECIDER_RANDOM)) {
-			decider = new RandomDecider();
-		} else if (deciderKind.equals(GemocModelLauncherConfigurationConstants.DECIDER_ASKUSER)) {
-			// use random as the only compatible decider
-			decider = new UserDecider(false);
-			
-		} else if (deciderKind.equals(GemocModelLauncherConfigurationConstants.DECIDER_ASKUSER_STEP_BY_STEP)) {
-			// use random as the only compatible decider
-			decider = new UserDecider(true);			
-		} else 	{
-			if (solver instanceof CcslSolver) {
-				// use solver proposition
-				decider = new CcslSolverDecider((CcslSolver) solver);
-			} else {
-				// use random as the only compatible decider
+		
+		switch(deciderKind)
+		{
+			case GemocModelLauncherConfigurationConstants.DECIDER_RANDOM:
 				decider = new RandomDecider();
-			}			
+				break;
+
+			case GemocModelLauncherConfigurationConstants.DECIDER_ASKUSER:
+				decider = new UserDecider(false);
+				break;
+
+			case GemocModelLauncherConfigurationConstants.DECIDER_ASKUSER_STEP_BY_STEP:
+				decider = new UserDecider(true);			
+				break;
+
+			default:
+				if (solver instanceof CcslSolver) {
+					// use solver proposition
+					decider = new CcslSolverDecider((CcslSolver) solver);
+				} else {
+					// use random as the only compatible decider
+					decider = new RandomDecider();
+				}				
+				break;
 		}
 		return decider;
 	}
