8bc9286ca70f82620df15e27b932c3e9a776b206
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-07-04 14:26:26 +0200

    fix sparsecheckout config in case of multiple folders

--- org/gemoc/gemoc_commons/org.gemoc.commons.root/configure_jenkinsgit4sparsecheckout.sh
+++ org/gemoc/gemoc_commons/org.gemoc.commons.root/configure_jenkinsgit4sparsecheckout.sh
@@ -2,6 +2,6 @@
 # simply add a call to this script from jenkins job
 git config core.sparsecheckout true
 echo "org/gemoc/gemoc_commons" > .git/info/sparse-checkout
-echo "org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.api" > .git/info/sparse-checkout
+echo "org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.api" >> .git/info/sparse-checkout
 git read-tree -m -u HEAD
 cd org/gemoc/gemoc_commons
