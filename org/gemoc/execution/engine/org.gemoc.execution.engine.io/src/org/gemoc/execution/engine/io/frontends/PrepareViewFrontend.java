96a231e21473e88a785b219fc1ebb6c031a9dae8
ftanguy <francois.tanguy@inria.fr>
2014-07-09 10:17:31 +0200

    remove unused imports

--- org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/frontends/PrepareViewFrontend.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/frontends/PrepareViewFrontend.java
@@ -8,8 +8,8 @@ import org.eclipse.ui.PlatformUI;
 import org.gemoc.commons.eclipse.ui.ViewHelper;
 import org.gemoc.execution.engine.core.ObservableBasicExecutionEngine;
 import org.gemoc.execution.engine.io.core.Frontend;
-import org.gemoc.execution.engine.io.views.EnginesStatusView;
 import org.gemoc.execution.engine.io.views.ExecutionTraceView;
+import org.gemoc.execution.engine.io.views.engine.EnginesStatusView;
 import org.gemoc.execution.engine.io.views.obeo.TimelineProvider;
 import org.gemoc.gemoc_language_workbench.api.core.GemocExecutionEngine;
 
