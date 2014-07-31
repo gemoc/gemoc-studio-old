eb41f680a8c911cc887fe018e334e51dd0cb7a62
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-07-31 11:01:54 +0200

    simplification of the code

--- org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.utils/src/org/gemoc/gemoc_language_workbench/utils/resourcevisitors/FileFinderVisitor.java
+++ org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.utils/src/org/gemoc/gemoc_language_workbench/utils/resourcevisitors/FileFinderVisitor.java
@@ -1,5 +1,8 @@
 package org.gemoc.gemoc_language_workbench.utils.resourcevisitors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
@@ -13,10 +16,22 @@ public class FileFinderVisitor implements IResourceVisitor {
 	}
 	
 	private String _fileExtension;
-	private IFile _file;
 	
+	protected List<IFile> _files = new ArrayList<IFile>();
+	
+	/**
+	 * @return the first file found, or null if not file found
+	 */
 	public IFile getFile() {
-		return _file;
+		if(_files.isEmpty()) return null;
+		return _files.get(0);
+	}
+	/** 
+	 * 
+	 * @return a list of the files found
+	 */
+	public List<IFile> getFiles() {
+		return _files;
 	}
 
 	@Override
@@ -24,7 +39,7 @@ public class FileFinderVisitor implements IResourceVisitor {
 		boolean mustVisitChild = true;
 		if(resource instanceof IFile){
 			if(resource.getFileExtension().equals(_fileExtension)){
-				_file = ((IFile) resource);
+				_files.add((IFile) resource);
 			}
 			mustVisitChild = false;
 		}
--- org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.utils/src/org/gemoc/gemoc_language_workbench/utils/resourcevisitors/GenmodelFileFinderResourceVisitor.java
+++ org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.utils/src/org/gemoc/gemoc_language_workbench/utils/resourcevisitors/GenmodelFileFinderResourceVisitor.java
@@ -1,30 +1,13 @@
 package org.gemoc.gemoc_language_workbench.utils.resourcevisitors;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IResourceVisitor;
-import org.eclipse.core.runtime.CoreException;
 
 /**
  * ResourceVisitor that look and gathers ecore files
  * @author dvojtise
  *
  */
-public class GenmodelFileFinderResourceVisitor implements IResourceVisitor{
-	public List<IFile> genmodelFiles = new ArrayList<IFile>(); 
-	@Override
-	public boolean visit(IResource resource) throws CoreException {
-		boolean mustVisitChild = true;
-		if(resource instanceof IFile){
-			if(resource.getFileExtension().equals("genmodel")){
-				genmodelFiles.add((IFile) resource);
-			}
-			mustVisitChild = false;
-		}
-		return mustVisitChild;
+public class GenmodelFileFinderResourceVisitor extends FileFinderVisitor{
+	public GenmodelFileFinderResourceVisitor() {
+		super("genmodel");
 	}
-	
 }
