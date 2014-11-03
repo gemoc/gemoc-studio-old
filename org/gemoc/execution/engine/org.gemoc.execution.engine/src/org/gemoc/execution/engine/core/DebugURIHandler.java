25bbc7e4c62ee65a1d5d094422aa66c855bb5602
Yvan Lussaud <yvan.lussaud@obeo.fr>
2014-11-03 15:36:02 +0100

    Fixed DebugURIHandler

--- org/gemoc/execution/engine/org.gemoc.execution.engine/src/org/gemoc/execution/engine/core/DebugURIHandler.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine/src/org/gemoc/execution/engine/core/DebugURIHandler.java
@@ -10,8 +10,8 @@ import org.eclipse.emf.ecore.resource.URIHandler;
 import org.eclipse.emf.ecore.resource.impl.URIHandlerImpl;
 
 /**
- * Creates {@link InputStream} from the worskpace and {@link OutputStream}
- * to nowhere. This prevent saving files.
+ * Creates {@link InputStream} from the worskpace and {@link OutputStream} to
+ * nowhere. This prevent saving files.
  * 
  * @author <a href="mailto:yvan.lussaud@obeo.fr">Yvan Lussaud</a>
  *
@@ -20,24 +20,18 @@ public final class DebugURIHandler extends URIHandlerImpl implements URIHandler 
 
 	@Override
 	public boolean canHandle(URI uri) {
-		return true;
+		return !uri.fileExtension().equals("trace");
 	}
 
 	@Override
 	public OutputStream createOutputStream(URI uri, Map<?, ?> options) throws IOException {
-		final OutputStream res;
-		
-		if (uri.fileExtension().equals("trace")) {
-			res = super.createOutputStream(uri, options);
-		} else {
-			res = new OutputStream() {
+		final OutputStream res = new OutputStream() {
 
-				@Override
-				public void write(int b) throws IOException {
-					// noting to do here
-				}
-			};
-		}
+			@Override
+			public void write(int b) throws IOException {
+				// noting to do here
+			}
+		};
 		return res;
 	}
 
