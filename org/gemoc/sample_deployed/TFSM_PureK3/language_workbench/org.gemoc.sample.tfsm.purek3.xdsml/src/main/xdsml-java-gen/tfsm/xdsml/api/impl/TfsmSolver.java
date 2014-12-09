a24e272531bba047ddcedfde6d630a18d0201502
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-12-09 17:09:14 +0100

    code executor of this sample isn't generated so we move it to the src/man/java folder

--- org/gemoc/sample_deployed/TFSM_PureK3/language_workbench/org.gemoc.sample.tfsm.purek3.xdsml/src/main/xdsml-java-gen/tfsm/xdsml/api/impl/TfsmSolver.java
+++
@@ -1,9 +0,0 @@
-/* GENERATED FILE, do not modify manually                                                    *
- * If you need to modify it, disable the generation in the BuildOptions of the project.xdsml */
-package tfsm.xdsml.api.impl;
-import org.gemoc.execution.engine.commons.solvers.ccsl.CcslSolver;
-public class TfsmSolver extends CcslSolver {
-	public TfsmSolver(){
-	    super();
-	}
-}
