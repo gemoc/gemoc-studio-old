125e02b1207a9a70515102206e743da0108f8aec
ftanguy <francois.tanguy@inria.fr>
2015-01-12 14:15:09 +0100

    Fix content of the map.

--- org/gemoc/sample_deployed/TFSM/language_workbench/org.gemoc.sample.tfsm.design/src/org/gemoc/sample/tfsm/design/services/TFSMAnimatorServices.java
+++ org/gemoc/sample_deployed/TFSM/language_workbench/org.gemoc.sample.tfsm.design/src/org/gemoc/sample/tfsm/design/services/TFSMAnimatorServices.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.gemoc.gemoc_language_workbench.extensions.sirius.services.AbstractGemocAnimatorServices;
+import org.gemoc.gemoc_modeling_workbench.ui.debug.sirius.services.AbstractGemocDebuggerServices.StringCouple;
 
 public class TFSMAnimatorServices extends AbstractGemocAnimatorServices {
 
 	@Override
 	protected List<StringCouple> getRepresentationRefreshList() {
 		final List<StringCouple> res = new ArrayList<StringCouple>();
+		
+		res.add(new StringCouple("TrafficControl","Debug"));
+		res.add(new StringCouple("Semaphore","Debug"));
 
-		res.add(new StringCouple("TFSM", "Animator"));
-
+		res.add(new StringCouple("TrafficControl","Animation"));
+		res.add(new StringCouple("Semaphore","Animation"));
+		
+		//res.add(new StringCouple("TFSM","Animator"));
+		
 		return res;
 	}
 
