e084f4efaf2c3f64230157715ea8c67e59576718
Yvan Lussaud <yvan.lussaud@obeo.fr>
2015-01-12 10:59:18 +0100

    Fixed TFSMAnimatorServices imports.

--- org/gemoc/sample_deployed/TFSM/language_workbench/org.gemoc.sample.tfsm.design/src/org/gemoc/sample/tfsm/design/services/TFSMAnimatorServices.java
+++ org/gemoc/sample_deployed/TFSM/language_workbench/org.gemoc.sample.tfsm.design/src/org/gemoc/sample/tfsm/design/services/TFSMAnimatorServices.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.gemoc.gemoc_language_workbench.extensions.sirius.services.AbstractGemocAnimatorServices;
-import org.gemoc.gemoc_modeling_workbench.ui.debug.sirius.services.AbstractGemocDebuggerServices.StringCouple;
 
 public class TFSMAnimatorServices extends AbstractGemocAnimatorServices {
 
 	@Override
 	protected List<StringCouple> getRepresentationRefreshList() {
 		final List<StringCouple> res = new ArrayList<StringCouple>();
-		
-		res.add(new StringCouple("TFSM","Animator"));
-		
+
+		res.add(new StringCouple("TFSM", "Animator"));
+
 		return res;
 	}
 
