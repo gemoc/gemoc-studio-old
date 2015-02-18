d2cd0cdec9d49ca807a1daafa0359be5748e784b
Yvan Lussaud <yvan.lussaud@obeo.fr>
2015-02-18 10:50:03 +0100

    Added breakpoint options.

--- org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/debug/sirius/action/GemocToggleBreakpoint.java
+++ org/gemoc/gemoc_modeling_workbench/org.gemoc.gemoc_modeling_workbench.ui/src/main/java/org/gemoc/gemoc_modeling_workbench/ui/debug/sirius/action/GemocToggleBreakpoint.java
@@ -1,7 +1,12 @@
 package org.gemoc.gemoc_modeling_workbench.ui.debug.sirius.action;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.ecore.EObject;
+import org.gemoc.gemoc_modeling_workbench.ui.breakpoint.GemocBreakpoint;
 import org.gemoc.gemoc_modeling_workbench.ui.launcher.Launcher;
 
+import fr.obeo.dsl.debug.ide.DSLBreakpoint;
+import fr.obeo.dsl.debug.ide.sirius.ui.DSLToggleBreakpointsUtils;
 import fr.obeo.dsl.debug.ide.sirius.ui.action.AbstractToggleBreakpointAction;
 
 public class GemocToggleBreakpoint extends AbstractToggleBreakpointAction {
@@ -11,4 +16,17 @@ public class GemocToggleBreakpoint extends AbstractToggleBreakpointAction {
 		return Launcher.MODEL_ID;
 	}
 
+	@Override
+	protected DSLToggleBreakpointsUtils createToggleBreakpointsUtils() {
+		return new DSLToggleBreakpointsUtils(getModelIdentifier()){
+			
+			@Override
+			protected DSLBreakpoint createBreakpoint(Object selected,
+					EObject instruction) throws CoreException {
+				return new GemocBreakpoint(identifier, instruction, true);
+			}
+			
+		};
+	}
+	
 }
