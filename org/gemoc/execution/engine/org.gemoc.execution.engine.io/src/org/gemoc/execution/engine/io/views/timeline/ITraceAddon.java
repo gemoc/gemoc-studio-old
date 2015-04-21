8c5afedd77e7f73e88a9910f23d053f022854930
Erwan Bousse <erwan.bousse@irisa.fr>
2015-04-21 11:27:22 +0200

    Removing useless dependency to trace API from ITraceAddon

--- org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/views/timeline/ITraceAddon.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine.io/src/org/gemoc/execution/engine/io/views/timeline/ITraceAddon.java
@@ -3,11 +3,7 @@ package org.gemoc.execution.engine.io.views.timeline;
 import org.eclipse.emf.ecore.EObject;
 import org.gemoc.gemoc_language_workbench.api.engine_addon.IEngineAddon;
 
-import fr.inria.diverse.trace.api.ITraceManager;
-
 public interface ITraceAddon extends IEngineAddon{
-
-	ITraceManager getTraceManager();
 
 	void goTo(EObject o1);
 
