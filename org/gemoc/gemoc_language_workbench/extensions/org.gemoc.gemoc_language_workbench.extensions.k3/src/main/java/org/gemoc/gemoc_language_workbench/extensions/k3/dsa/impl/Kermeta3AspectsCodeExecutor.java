b060d30dc5c3cc3ac725644cf84d662f1c1eaccc
Erwan Bousse <erwan.bousse@irisa.fr>
2015-04-30 08:38:47 +0200

    Adding to the Kermeta3AspectsCodeExecution the capability to consider multiple aspects per class in the properties file (required for PlainK3 to work, since it has visitor + actions in separate aspects.

--- org/gemoc/gemoc_language_workbench/extensions/org.gemoc.gemoc_language_workbench.extensions.k3/src/main/java/org/gemoc/gemoc_language_workbench/extensions/k3/dsa/impl/Kermeta3AspectsCodeExecutor.java
+++ org/gemoc/gemoc_language_workbench/extensions/org.gemoc.gemoc_language_workbench.extensions.k3/src/main/java/org/gemoc/gemoc_language_workbench/extensions/k3/dsa/impl/Kermeta3AspectsCodeExecutor.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
 import org.gemoc.execution.engine.trace.gemoc_execution_trace.MSEOccurrence;
@@ -32,70 +33,63 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 	protected IK3DSAExecutorClassLoader k3DSAExecutorClassLoader;
 	protected String bundleSymbolicName;
 
-	public Kermeta3AspectsCodeExecutor(
-			IK3DSAExecutorClassLoader k3DSAExecutorClassLoader,
-			String bundleSymbolicName) {
+	public Kermeta3AspectsCodeExecutor(IK3DSAExecutorClassLoader k3DSAExecutorClassLoader, String bundleSymbolicName) {
 		// this.classLoader = classLoader;
 		this.k3DSAExecutorClassLoader = k3DSAExecutorClassLoader;
 		this.bundleSymbolicName = bundleSymbolicName;
 	}
 
-	
 	@Override
-	public Object execute(MSEOccurrence mseOccurrence) throws CodeExecutionException 
-	{
+	public Object execute(MSEOccurrence mseOccurrence) throws CodeExecutionException {
 		Object caller = mseOccurrence.getMse().getCaller();
 		String methodName = mseOccurrence.getMse().getAction().getName();
 		return internal_execute(caller, methodName, mseOccurrence.getParameters(), mseOccurrence);
 	}
 
 	@Override
-	public Object execute(Object caller, String methodName, List<Object> parameters) throws CodeExecutionException 
-	{
+	public Object execute(Object caller, String methodName, List<Object> parameters) throws CodeExecutionException {
 		return internal_execute(caller, methodName, parameters, null);
 	}
-	
-	private Object internal_execute(Object caller, String methodName, Collection<Object> parameters, MSEOccurrence mseOccurrence) throws CodeExecutionException 
-	{
+
+	private Object internal_execute(Object caller, String methodName, Collection<Object> parameters,
+			MSEOccurrence mseOccurrence) throws CodeExecutionException {
 		ArrayList<Object> staticParameters = new ArrayList<Object>();
 		staticParameters.add(caller);
-		if (parameters != null) 
-		{
+		if (parameters != null) {
 			staticParameters.addAll(parameters);
 		}
 		Method bestApplicableMethod = getBestApplicableMethod(caller, methodName, staticParameters);
 		if (bestApplicableMethod == null)
-			throw new CodeExecutionException("static class not found or method not found when calling "+methodName+ " on "+caller+". MSEOccurence=" + mseOccurrence, mseOccurrence, false); 
-		
+			throw new CodeExecutionException("static class not found or method not found when calling " + methodName
+					+ " on " + caller + ". MSEOccurence=" + mseOccurrence, mseOccurrence, false);
+
 		Object[] args = new Object[0];
-		if (staticParameters != null) 
-		{
+		if (staticParameters != null) {
 			args = staticParameters.toArray();
 		}
 		Object result = null;
 		try {
 			result = bestApplicableMethod.invoke(null, args);
-		} catch (IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) 
-		{
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
-			throw new CodeExecutionException("Exception caught during execution of a call, see inner exception.", e, mseOccurrence, true);
+			throw new CodeExecutionException("Exception caught during execution of a call, see inner exception.", e,
+					mseOccurrence, true);
 		}
 		return result;
 	}
-	
-	private Method getBestApplicableMethod(Object caller, 
-											String methodName,
-											List<Object> parameters)
-	{
-		Class<?> staticHelperClass = getStaticHelperClass(caller);
-		if (staticHelperClass == null)
-		{
+
+	private Method getBestApplicableMethod(Object caller, String methodName, List<Object> parameters) {
+		Set<Class<?>> staticHelperClasses = getStaticHelperClasses(caller);
+		if (staticHelperClasses == null || staticHelperClasses.isEmpty()) {
 			return null;
 		}
-		return getFirstApplicableMethod(staticHelperClass, methodName, parameters);	
+		for (Class<?> c : staticHelperClasses) {
+			Method m = getFirstApplicableMethod(c, methodName, parameters);
+			if (m != null)
+				return m;
+		}
+		return null;
 	}
-
 
 	/**
 	 * return the first compatible method, goes up the inheritance hierarchy
@@ -105,32 +99,26 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 	 * @param parameters
 	 * @return
 	 */
-	protected Method getFirstApplicableMethod(Class<?> staticHelperClass,
-			String methodName, List<Object> parameters) {
+	protected Method getFirstApplicableMethod(Class<?> staticHelperClass, String methodName, List<Object> parameters) {
 
 		Method[] methods = staticHelperClass.getDeclaredMethods();
 		for (Method method : methods) {
 			Class<?>[] evaluatedMethodParamTypes = method.getParameterTypes();
-			if (method.getName().equals(methodName)
-					&& evaluatedMethodParamTypes.length == parameters.size()) {
+			if (method.getName().equals(methodName) && evaluatedMethodParamTypes.length == parameters.size()) {
 				boolean isAllParamCompatible = true;
 				for (int i = 0; i < evaluatedMethodParamTypes.length; i++) {
 					Object p = parameters.get(i);
 					if (evaluatedMethodParamTypes[i].isPrimitive()) {
-						
-						if (evaluatedMethodParamTypes[i].equals(Integer.TYPE)
-							&& !Integer.class.isInstance(p)) {
+
+						if (evaluatedMethodParamTypes[i].equals(Integer.TYPE) && !Integer.class.isInstance(p)) {
 							isAllParamCompatible = false;
-							break;							
+							break;
+						} else if (evaluatedMethodParamTypes[i].equals(Boolean.TYPE) && !Boolean.class.isInstance(p)) {
+							isAllParamCompatible = false;
+							break;
 						}
-						else if (evaluatedMethodParamTypes[i].equals(Boolean.TYPE)
-								&& !Boolean.class.isInstance(p)) {
-								isAllParamCompatible = false;
-								break;							
-						}
-						
-					} 
-					else if (!evaluatedMethodParamTypes[i].isInstance(p)) {
+
+					} else if (!evaluatedMethodParamTypes[i].isInstance(p)) {
 						isAllParamCompatible = false;
 						break;
 					}
@@ -153,19 +141,15 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 	 * aspects, and deals with it as a list of applicable static classes)
 	 * 
 	 */
-	protected Class<?> getStaticHelperClass(Object target) {
-		List<Class<?>> allPossibleInterfaces = getInterfacesClassOfEObjectOrClass(
-				target);
+	protected Set<Class<?>> getStaticHelperClasses(Object target) {
+		List<Class<?>> allPossibleInterfaces = getInterfacesClassOfEObjectOrClass(target);
 
-		String searchedPropertyFileName = "/META-INF/xtend-gen/"
-				+ bundleSymbolicName + ".k3_aspect_mapping.properties";
+		String searchedPropertyFileName = "/META-INF/xtend-gen/" + bundleSymbolicName + ".k3_aspect_mapping.properties";
 		Properties properties = new Properties();
-		InputStream inputStream = k3DSAExecutorClassLoader
-				.getResourceAsStream(searchedPropertyFileName);
+		InputStream inputStream = k3DSAExecutorClassLoader.getResourceAsStream(searchedPropertyFileName);
 		if (inputStream == null) {
 			try {
-				inputStream = org.eclipse.core.runtime.Platform
-						.getBundle(bundleSymbolicName)
+				inputStream = org.eclipse.core.runtime.Platform.getBundle(bundleSymbolicName)
 						.getEntry(searchedPropertyFileName).openStream();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -173,37 +157,42 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 				return null;
 			}
 		}
-		String possibleStaticClassName = null;
+		String possibleStaticClassesNames = null;
 		try {
 			if (inputStream != null) {
 				properties.load(inputStream);
-				for (int i = 0 ; i < allPossibleInterfaces.size() ; i++) {
-					possibleStaticClassName = properties
-						.getProperty(allPossibleInterfaces.get(i).getCanonicalName());
-					// Break so that the aspect is applied on the most precise type
-					if (possibleStaticClassName != null)
-						break ;
+				for (int i = 0; i < allPossibleInterfaces.size(); i++) {
+					possibleStaticClassesNames = properties
+							.getProperty(allPossibleInterfaces.get(i).getCanonicalName());
+					// Break so that the aspect is applied on the most precise
+					// type
+					if (possibleStaticClassesNames != null)
+						break;
 				}
 			}
 		} catch (IOException e) {
 			// TODO report for debug that no mapping was found
 			return null;
 		}
-		if (possibleStaticClassName == null) {
+		if (possibleStaticClassesNames == null) {
 			return null;
-
 		}
 
-		try { // Class.forName(possibleStaticClassName)
-				// Class.forName(possibleStaticClassName, true, classLoader);
-			return k3DSAExecutorClassLoader
-					.getClassForName(possibleStaticClassName);
-		} catch (ClassNotFoundException e) {
-			Activator.getMessagingSystem().error(
-					"ClassNotFoundException, see Error Log View",
-					Activator.PLUGIN_ID, e);
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		ClassNotFoundException possibleException = null;
+		for (String possibleName : possibleStaticClassesNames.replaceAll(" ", "").split(",")) {
+			try {
+				classes.add(k3DSAExecutorClassLoader.getClassForName(possibleName));
+			} catch (ClassNotFoundException e) {
+				possibleException = e;
+			}
 		}
-		return null;
+		if (classes.isEmpty()) {
+			Activator.getMessagingSystem().error("ClassNotFoundException, see Error Log View", Activator.PLUGIN_ID,
+					possibleException);
+		}
+
+		return classes;
 	}
 
 	/**
@@ -234,12 +223,12 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 
 			// @author tdegueul
 			// Since aspects may target both base / extended metamodel classes
-			// we need to retrieve the complete hierarchy of possible applications
+			// we need to retrieve the complete hierarchy of possible
+			// applications
 			List<Class<?>> interfaces = getAllInterfaces(o.getClass());
 			for (int i = 0; i < interfaces.size(); i++) {
 				Class<?> interfac = interfaces.get(i);
-				if (interfac.getSimpleName().equals(
-						((EObject) o).eClass().getName())) {
+				if (interfac.getSimpleName().equals(((EObject) o).eClass().getName())) {
 					possibleInterfaces.add(interfac);
 				}
 			}
@@ -252,54 +241,56 @@ public class Kermeta3AspectsCodeExecutor implements ICodeExecutor {
 
 	}
 
-	// The two following methods are copied from org.apache.commons.lang.ClassUtils
+	// The two following methods are copied from
+	// org.apache.commons.lang.ClassUtils
 	private static List<Class<?>> getAllInterfaces(Class<?> cls) {
-        if (cls == null) {
-            return null;
-        }
+		if (cls == null) {
+			return null;
+		}
 
-        LinkedHashSet<Class<?>> interfacesFound = new LinkedHashSet<Class<?>>();
-        getAllInterfaces(cls, interfacesFound);
+		LinkedHashSet<Class<?>> interfacesFound = new LinkedHashSet<Class<?>>();
+		getAllInterfaces(cls, interfacesFound);
 
-        return new ArrayList<Class<?>>(interfacesFound);
-    }
-
-    private static void getAllInterfaces(Class<?> cls, HashSet<Class<?>> interfacesFound) {
-        while (cls != null) {
-            Class<?>[] interfaces = cls.getInterfaces();
-
-            for (Class<?> i : interfaces) {
-                if (interfacesFound.add(i)) {
-                    getAllInterfaces(i, interfacesFound);
-                }
-            }
-
-            cls = cls.getSuperclass();
-         }
-     }
-
-    @Override
-	public String getExcutorID() {
-		return this.getClass().getSimpleName()+"["+bundleSymbolicName+"]";
+		return new ArrayList<Class<?>>(interfacesFound);
 	}
 
-//	@Override
-//	public boolean canExecute(ActionCall call) 
-//	{
-//		return getBestApplicableMethod(call.getTriggeringEvent().getCaller(), 
-//										call.getTriggeringEvent().getAction().getName(), 
-//										call.getParameters(), 
-//										call) != null;
-//	}
-//
-//
-//	@Override
-//	public boolean canExecute(Object caller, String methodName, List<Object> parameters) 
-//	{
-//		return getBestApplicableMethod(caller, 
-//										methodName, 
-//										parameters, 
-//										null) != null;
-//	}
+	private static void getAllInterfaces(Class<?> cls, HashSet<Class<?>> interfacesFound) {
+		while (cls != null) {
+			Class<?>[] interfaces = cls.getInterfaces();
+
+			for (Class<?> i : interfaces) {
+				if (interfacesFound.add(i)) {
+					getAllInterfaces(i, interfacesFound);
+				}
+			}
+
+			cls = cls.getSuperclass();
+		}
+	}
+
+	@Override
+	public String getExcutorID() {
+		return this.getClass().getSimpleName() + "[" + bundleSymbolicName + "]";
+	}
+
+	// @Override
+	// public boolean canExecute(ActionCall call)
+	// {
+	// return getBestApplicableMethod(call.getTriggeringEvent().getCaller(),
+	// call.getTriggeringEvent().getAction().getName(),
+	// call.getParameters(),
+	// call) != null;
+	// }
+	//
+	//
+	// @Override
+	// public boolean canExecute(Object caller, String methodName, List<Object>
+	// parameters)
+	// {
+	// return getBestApplicableMethod(caller,
+	// methodName,
+	// parameters,
+	// null) != null;
+	// }
 
 }
