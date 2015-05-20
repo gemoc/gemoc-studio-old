c76509e6baaadee4dfeb00fb25039bb0e9c8ffe0
Erwan Bousse <erwan.bousse@irisa.fr>
2015-05-20 18:54:50 +0200

    Changing the Merger to fix restoreState duplicates
    The hack relies on a second custom merger that bypasses the
    reference merger, so that we can override the object creation
    behavior to make sure that there are no duplicates.

--- org/gemoc/execution/engine/org.gemoc.execution.engine.commons/src/org/gemoc/execution/engine/commons/trace/Merger.java
+++ org/gemoc/execution/engine/org.gemoc.execution.engine.commons/src/org/gemoc/execution/engine/commons/trace/Merger.java
@@ -3,15 +3,47 @@ package org.gemoc.execution.engine.commons.trace;
 import org.eclipse.emf.common.util.Monitor;
 import org.eclipse.emf.compare.Diff;
 import org.eclipse.emf.compare.merge.AbstractMerger;
+import org.eclipse.emf.compare.merge.ReferenceChangeMerger;
 import org.eclipse.emf.compare.rcp.EMFCompareRCPPlugin;
+import org.eclipse.emf.compare.utils.EMFCompareCopier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 class Merger extends AbstractMerger {
 
-	public Merger() 
-	{
-		setRegistry(EMFCompareRCPPlugin.getDefault().getMergerRegistry());
+	/**
+	 * We want to bypass the reference merger that is eventually called by the 
+	 * abstract merger, so we create this merger with a very high ranking.
+	 * 
+	 * The goal of the bypass is th replace the operation createCopy, so that
+	 * objects are not created multiple times.
+	 * 
+	 * @author ebousse
+	 *
+	 */
+	public static class CustomReferenceMerger extends ReferenceChangeMerger {
+
+		final EcoreUtil.Copier copier = new EMFCompareCopier();
+
+		protected EObject createCopy(EObject referenceObject) {
+
+			if (copier.containsKey(referenceObject))
+				return copier.get(referenceObject);
+			return copier.copy(referenceObject);
+		}
+
+		@Override
+		public int getRanking() {
+			return Integer.MAX_VALUE;
+		}
+
 	}
-	
+
+	public Merger() {
+		setRegistry(EMFCompareRCPPlugin.getDefault().getMergerRegistry());
+		getRegistry().add(new CustomReferenceMerger());
+	}
+
 	@Override
 	public boolean isMergerFor(Diff target) {
 		// TODO Auto-generated method stub
