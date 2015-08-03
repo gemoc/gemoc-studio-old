6ea04eee64b7c41ec31ded689d03671c8dd7ab40
Erwan Bousse <erwan.bousse@irisa.fr>
2015-08-03 09:48:22 +0200

    Add unregister in K3Engine, comments, moving stuff

--- org/gemoc/gemoc_language_workbench/extensions/org.gemoc.gemoc_language_workbench.extensions.k3/src/main/java/org/gemoc/gemoc_language_workbench/extensions/k3/PlainK3ExecutionEngine.java
+++ org/gemoc/gemoc_language_workbench/extensions/org.gemoc.gemoc_language_workbench.extensions.k3/src/main/java/org/gemoc/gemoc_language_workbench/extensions/k3/PlainK3ExecutionEngine.java
@@ -18,10 +18,27 @@ public class PlainK3ExecutionEngine extends AbstractDeterministicExecutionEngine
 
 	private Runnable _entryPoint;
 
+	/**
+	 * Constructs a PlainK3 execution engine using an entry point (~ a main
+	 * operation) The entrypoint will register itself as a StepManager into the
+	 * K3 step manager registry, and unregister itself at the end. As a
+	 * StepManager, the PlainK3ExecutionEngine will receive callbacks through
+	 * its "executeStep" operation.
+	 * 
+	 * @param context
+	 *            The GEMOC execution context.
+	 * @param caller
+	 *            The Main object used to call the main operation entry point.
+	 * @param method
+	 *            The main operation entry point. Should have a single
+	 *            parameter, which is the root of the executed model.
+	 * @param parameters
+	 *            The parameters given to the main operations. Should contain
+	 *            one object which is the root of the model.
+	 */
 	public PlainK3ExecutionEngine(final IExecutionContext context, final Object caller, final Method method,
 			final ArrayList<Object> parameters) {
 		super(context);
-
 		_entryPoint = new Runnable() {
 			@Override
 			public void run() {
@@ -30,6 +47,8 @@ public class PlainK3ExecutionEngine extends AbstractDeterministicExecutionEngine
 					method.invoke(caller, parameters.get(0));
 				} catch (Exception e) {
 					throw new RuntimeException(e);
+				} finally {
+					StepManagerRegistry.getInstance().unregisterManager(PlainK3ExecutionEngine.this);
 				}
 			}
 		};
@@ -39,6 +58,44 @@ public class PlainK3ExecutionEngine extends AbstractDeterministicExecutionEngine
 	@Override
 	public Runnable getEntryPoint() {
 		return _entryPoint;
+	}
+
+	@Override
+	/*
+	 * This is the operation called from K3 code. We use this callback to pass
+	 * the command to the generic executeOperation operation. (non-Javadoc)
+	 * 
+	 * @see fr.inria.diverse.k3.al.annotationprocessor.stepmanager.IStepManager#
+	 * executeStep(java.lang.Object,
+	 * fr.inria.diverse.k3.al.annotationprocessor.stepmanager.StepCommand,
+	 * java.lang.String)
+	 */
+	public void executeStep(Object caller, final StepCommand command, String methodName) {
+		executeOperation(caller, methodName, new Runnable() {
+			@Override
+			public void run() {
+				command.execute();
+			}
+		});
+	}
+
+	@Override
+	/*
+	 * This is the operation used to act as a StepManager in K3. We return true
+	 * if we have the same editing domain as the object. (non-Javadoc)
+	 * 
+	 * @see
+	 * fr.inria.diverse.k3.al.annotationprocessor.stepmanager.IStepManager#canHandle
+	 * (java.lang.Object)
+	 */
+	public boolean canHandle(Object caller) {
+		if (caller instanceof EObject) {
+			EObject caller_cast = (EObject) caller;
+			org.eclipse.emf.transaction.TransactionalEditingDomain editingDomain = getEditingDomain(caller_cast);
+			return editingDomain == this.editingDomain;
+
+		}
+		return false;
 	}
 
 	private static TransactionalEditingDomain getEditingDomain(EObject o) {
@@ -52,27 +109,6 @@ public class PlainK3ExecutionEngine extends AbstractDeterministicExecutionEngine
 			return (InternalTransactionalEditingDomain) edomain;
 		else
 			return null;
-	}
-
-	@Override
-	public void executeStep(Object caller, final StepCommand command, String methodName) {
-		executeOperation(caller, methodName, new Runnable() {
-			@Override
-			public void run() {
-				command.execute();
-			}
-		});
-	}
-
-	@Override
-	public boolean canHandle(Object caller) {
-		if (caller instanceof EObject) {
-			EObject caller_cast = (EObject) caller;
-			org.eclipse.emf.transaction.TransactionalEditingDomain editingDomain = getEditingDomain(caller_cast);
-			return editingDomain == this.editingDomain;
-
-		}
-		return false;
 	}
 
 }
