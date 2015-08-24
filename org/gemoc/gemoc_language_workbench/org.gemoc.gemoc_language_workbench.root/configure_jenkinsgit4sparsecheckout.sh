ede19dbf5fd42942e29d8b05b7a940e647a577bd
Didier Vojtisek <didier.vojtisek@inria.fr>
2015-08-24 14:32:37 +0200

    removing deprecated feature projects and pom.xml

--- org/gemoc/gemoc_language_workbench/org.gemoc.gemoc_language_workbench.root/configure_jenkinsgit4sparsecheckout.sh
+++
@@ -1,8 +0,0 @@
-# configure git for sparse checkout so we can optimize diskusage (ie. checkout only required folders)
-# simply add a call to this script from jenkins job
-git config core.sparsecheckout true
-echo "org/gemoc/gemoc_language_workbench" > .git/info/sparse-checkout
-echo "org/gemoc/execution" >> .git/info/sparse-checkout
-echo "org/gemoc/sample_deployed" >> .git/info/sparse-checkout
-git read-tree -m -u HEAD
-# cd org/gemoc/gemoc_language_workbench
