5bf716ca5d3b09065b95ed4648ff21b849dc1137
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-01-08 09:27:02 +0100

    adapting official samples to refactored studio

--- official_samples/TFSM/language_workbench/org.gemoc.sample.tfsm.xdsml/src/main/java/org/gemoc/sample/tfsm/xdsml/api/impl/TfsmCodeExecutor.java
+++ official_samples/TFSM/language_workbench/org.gemoc.sample.tfsm.xdsml/src/main/java/org/gemoc/sample/tfsm/xdsml/api/impl/TfsmCodeExecutor.java
@@ -2,16 +2,16 @@
  * If you need to modify it, disable the generation in the BuildOptions of the project.xdsml */
 package org.gemoc.sample.tfsm.xdsml.api.impl;
 
-import org.gemoc.executionengine.ccsljava.engine.dsa.executors.CodeExecutorDispatcher;
+import org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.CodeExecutorDispatcher;
 
 public class TfsmCodeExecutor extends CodeExecutorDispatcher 
-		implements org.gemoc.gemoc_language_workbench.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
+		implements org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
 	public TfsmCodeExecutor(){
 	    // add K3 DSA specific executor
-		addExecutor(new org.gemoc.gemoc_language_workbench.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
 			"org.gemoc.sample.tfsm.k3dsa"));
 		// fall back executor : search classic java method
-		addExecutor(new org.gemoc.executionengine.ccsljava.engine.dsa.executors.JavaCodeExecutor());
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.JavaCodeExecutor());
 	}
    @Override
 	public Class<?> getClassForName(String className) throws ClassNotFoundException {
--- official_samples/TFSM/language_workbench/org.gemoc.sample.tfsm.xdsml/src/main/xdsml-java-gen/tfsm/xdsml/api/impl/TfsmCodeExecutor.java
+++ official_samples/TFSM/language_workbench/org.gemoc.sample.tfsm.xdsml/src/main/xdsml-java-gen/tfsm/xdsml/api/impl/TfsmCodeExecutor.java
@@ -1,15 +1,15 @@
 /* GENERATED FILE, do not modify manually                                                    *
  * If you need to modify it, disable the generation in the BuildOptions of the project.xdsml */
 package tfsm.xdsml.api.impl;
-import org.gemoc.executionengine.ccsljava.engine.dsa.executors.CodeExecutorDispatcher;
+import org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.CodeExecutorDispatcher;
 public class TfsmCodeExecutor extends CodeExecutorDispatcher 
-		implements org.gemoc.gemoc_language_workbench.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
+		implements org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.api.IK3DSAExecutorClassLoader  {
 	public TfsmCodeExecutor(){
 	    // add K3 DSA specific executor
-		addExecutor(new org.gemoc.gemoc_language_workbench.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.extensions.k3.dsa.impl.Kermeta3AspectsCodeExecutor(this,
 			"org.gemoc.sample.tfsm.tfsmextended.aspect"));
 		// fall back executor : search classic java method
-		addExecutor(new org.gemoc.executionengine.ccsljava.engine.dsa.executors.JavaCodeExecutor());
+		addExecutor(new org.gemoc.execution.concurrent.ccsljavaengine.dsa.executors.JavaCodeExecutor());
 	}
    @Override
 	public Class<?> getClassForName(String className) throws ClassNotFoundException {
