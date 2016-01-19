4a79b360b7bab39d1c3e4667f9b32234ebf242dd
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-01-19 15:12:36 +0100

    manually fixed the tfsm concurrent code generated by melange in order to have a working tfsm in the modeling workbench

---
+++ official_samples/TFSM/language_workbench/org.gemoc.sample.tfsm.xdsml/src-gen/org/gemoc/sample/tfsm/xdsml/tfsmconcurrentmt/tfsm/impl/TfsmFactoryImpl.java
@@ -0,0 +1,184 @@
+/**
+ */
+package org.gemoc.sample.tfsm.xdsml.tfsmconcurrentmt.tfsm.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import org.gemoc.sample.tfsm.xdsml.tfsmconcurrentmt.tfsm.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TfsmFactoryImpl extends EFactoryImpl implements TfsmFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static TfsmFactory init() {
+		try {
+			TfsmFactory theTfsmFactory = (TfsmFactory)EPackage.Registry.INSTANCE.getEFactory(TfsmPackage.eNS_URI);
+			if (theTfsmFactory != null) {
+				return theTfsmFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new TfsmFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TfsmFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case TfsmPackage.TFSM: return createTFSM();
+			case TfsmPackage.STATE: return createState();
+			case TfsmPackage.TRANSITION: return createTransition();
+			case TfsmPackage.NAMED_ELEMENT: return createNamedElement();
+			case TfsmPackage.TEMPORAL_GUARD: return createTemporalGuard();
+			case TfsmPackage.EVENT_GUARD: return createEventGuard();
+			case TfsmPackage.FSM_EVENT: return createFSMEvent();
+			case TfsmPackage.FSM_CLOCK: return createFSMClock();
+			case TfsmPackage.TIMED_SYSTEM: return createTimedSystem();
+			case TfsmPackage.EVALUATE_GUARD: return createEvaluateGuard();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TFSM createTFSM() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public State createState() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Transition createTransition() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NamedElement createNamedElement() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TemporalGuard createTemporalGuard() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EventGuard createEventGuard() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public FSMEvent createFSMEvent() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public FSMClock createFSMClock() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TimedSystem createTimedSystem() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EvaluateGuard createEvaluateGuard() {
+		throw new UnsupportedOperationException("Cannot invoke element creation on a model type");
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public TfsmPackage getTfsmPackage() {
+		return (TfsmPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static TfsmPackage getPackage() {
+		return TfsmPackage.eINSTANCE;
+	}
+
+} //TfsmFactoryImpl
