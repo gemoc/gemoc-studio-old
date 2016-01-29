e6ef1ef3b37ab24145b94a7d27465b797fe119ca
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-01-29 10:15:51 +0100

    make sure to write the file even if the file exists

--- commons/plugins/org.gemoc.commons.eclipse/src/org/gemoc/commons/eclipse/core/resources/GFile.java
+++ commons/plugins/org.gemoc.commons.eclipse/src/org/gemoc/commons/eclipse/core/resources/GFile.java
@@ -4,12 +4,17 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 public class GFile {
 
 	public static void writeFile(IFile file, String fileContent) throws CoreException{
 		IProject project = file.getProject();
-		Project.createFile(project, file, fileContent);
+		if(file.exists()){
+			Project.setFileContent(project, file, fileContent, new NullProgressMonitor());
+		} else {
+			Project.createFile(project, file, fileContent);
+		}
 	}
 	
 	public static void addMarker(IFile file, String markerType, String message, int severity) throws CoreException {
