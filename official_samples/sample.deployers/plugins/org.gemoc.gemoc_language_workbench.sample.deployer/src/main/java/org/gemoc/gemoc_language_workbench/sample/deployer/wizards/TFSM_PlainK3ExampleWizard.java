5b6e0532b9acdf62b8f61e9545d3f9e43a3a199f
Didier Vojtisek <didier.vojtisek@inria.fr>
2016-09-05 17:40:17 +0200

    plain K" TFSM is now base on generated ecore from melange

--- official_samples/sample.deployers/plugins/org.gemoc.gemoc_language_workbench.sample.deployer/src/main/java/org/gemoc/gemoc_language_workbench/sample/deployer/wizards/TFSM_PlainK3ExampleWizard.java
+++ official_samples/sample.deployers/plugins/org.gemoc.gemoc_language_workbench.sample.deployer/src/main/java/org/gemoc/gemoc_language_workbench/sample/deployer/wizards/TFSM_PlainK3ExampleWizard.java
@@ -26,11 +26,11 @@ public class TFSM_PlainK3ExampleWizard
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.design.zip", "org.gemoc.sample.tfsm.plaink3.design"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.dsa.zip", "org.gemoc.sample.tfsm.plaink3.dsa"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.model.zip", "org.gemoc.sample.tfsm.plaink3.model"));
-		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.model.edit.zip", "org.gemoc.sample.tfsm.plaink3.model.edit"));
-		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.model.editor.zip", "org.gemoc.sample.tfsm.plaink3.model.editor"));
-		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.tfsm.trace.zip", "org.gemoc.sample.tfsm.plaink3.tfsm.trace"));
 		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xdsml.zip", "org.gemoc.sample.tfsm.plaink3.xdsml"));
-		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xdsml.tfsm.zip", "org.gemoc.sample.tfsm.plaink3.xdsml.tfsm"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xdsml.xtfsm.zip", "org.gemoc.sample.tfsm.plaink3.xdsml.xtfsm"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xdsml.xtfsm.edit.zip", "org.gemoc.sample.tfsm.plaink3.xdsml.xfsm.edit"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xdsml.xtfsm.editor.zip", "org.gemoc.sample.tfsm.plaink3.xdsml.xtfsm.editor"));
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.gemoc.sample.tfsm.plaink3.xtfsm.trace.zip", "org.gemoc.sample.tfsm.plaink3.xtfsm.trace"));
 		return projects;
 	}
 
