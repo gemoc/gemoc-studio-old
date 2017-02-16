ae83e31064dfdeaefa9c9edde873781acd018f01
Didier Vojtisek <didier.vojtisek@inria.fr>
2017-02-16 15:27:10 +0100

    workaround Melange's bug #66
    https://github.com/diverse-project/melange/issues/66 
    removing strange gitignore and add missing file in repository

---
+++ official_samples/LegacyFSM/language_workbench/org.gemoc.sample.legacyfsm.xsfsm/src-gen/org/gemoc/sample/legacyfsm/xsfsm/xsfsm/adapters/xsfsmmt/fsm/FsmFactoryAdapter.java
@@ -0,0 +1,41 @@
+package org.gemoc.sample.legacyfsm.xsfsm.xsfsm.adapters.xsfsmmt.fsm;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsm.adapters.xsfsmmt.XSFSMMTAdaptersFactory;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.FsmFactory;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.FsmPackage;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.State;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.StateMachine;
+import org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.Transition;
+
+@SuppressWarnings("all")
+public class FsmFactoryAdapter extends EFactoryImpl implements FsmFactory {
+  private XSFSMMTAdaptersFactory adaptersFactory = org.gemoc.sample.legacyfsm.xsfsm.xsfsm.adapters.xsfsmmt.XSFSMMTAdaptersFactory.getInstance();
+  
+  private org.gemoc.sample.legacyfsm.xsfsm.xsfsm.fsm.FsmFactory fsmAdaptee = org.gemoc.sample.legacyfsm.xsfsm.xsfsm.fsm.FsmFactory.eINSTANCE;
+  
+  @Override
+  public StateMachine createStateMachine() {
+    return adaptersFactory.createStateMachineAdapter(fsmAdaptee.createStateMachine(), null);
+  }
+  
+  @Override
+  public State createState() {
+    return adaptersFactory.createStateAdapter(fsmAdaptee.createState(), null);
+  }
+  
+  @Override
+  public Transition createTransition() {
+    return adaptersFactory.createTransitionAdapter(fsmAdaptee.createTransition(), null);
+  }
+  
+  @Override
+  public EPackage getEPackage() {
+    return getFsmPackage();
+  }
+  
+  public FsmPackage getFsmPackage() {
+    return org.gemoc.sample.legacyfsm.xsfsm.xsfsmmt.fsm.FsmPackage.eINSTANCE;
+  }
+}
