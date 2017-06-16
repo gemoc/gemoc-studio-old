178eb94d20e50caf958fd616dc2d31928faa9ef1
dvojtise <didier.vojtisek@inria.fr>
2017-06-16 16:46:29 +0200

    renaming org.gemoc.gemoc_language_workbench.documentation in org.eclipse.gemoc.language_workbench.documentation

--- gemoc_studio/plugins/org.eclipse.gemoc.language_workbench.documentation/src/org/eclipse/gemoc/language_workbench/documentation/Activator.java
+++ gemoc_studio/plugins/org.eclipse.gemoc.language_workbench.documentation/src/org/eclipse/gemoc/language_workbench/documentation/Activator.java
@@ -1,4 +1,4 @@
-package org.gemoc.gemoc_language_workbench.documentation;
+package org.eclipse.gemoc.language_workbench.documentation;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.gemoc.gemoc_language_workbench.documentation"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.gemoc.language_workbench.documentation"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
